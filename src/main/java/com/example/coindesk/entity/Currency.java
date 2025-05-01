3b977a3d204dced7226dd6774aa2d45c134759e2
DESKTOP-T99A5CD\B450 <A>
2025-05-02 00:29:46 +0800

    feat: 新增專案
    1. 幣別與中文名稱的CRUD

---
+++ src/main/java/com/example/coindesk/entity/Currency.java
@@ -0,0 +1,35 @@
+package com.example.coindesk.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "currency")
+public class Currency {
+
+	@Id
+	@Column(name = "code")
+	private String code;
+
+	@Column(name = "chinese_name")
+	private String chineseName;
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getChineseName() {
+		return chineseName;
+	}
+
+	public void setChineseName(String chineseName) {
+		this.chineseName = chineseName;
+	}
+
+}
